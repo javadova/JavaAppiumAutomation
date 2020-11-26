ba5cc30681b3fa654929674eecea3ca5347735cf
Elvira Javadova <javadova@wamba.com>
2020-11-26 14:17:37 +0300

    test 3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -23,4 +23,15 @@ public class MainClassTest extends MainClass {
         }
     }
 
-}
+    @Test
+    public void testGetClassString() {
+        String phrase = this.getClassString();
+        if (phrase.contains("Hello")) {
+            System.out.println(phrase);
+        } else if (phrase.contains("hello")) {
+                System.out.println(phrase);
+            } else {
+                System.out.println("Test Failed " + phrase + " не содержит Hello или hello");
+            }
+        }
+    }
